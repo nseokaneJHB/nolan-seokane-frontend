a26813950a4c3d47ad6507f81e75442691635318
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-10-12 14:34:51 +0200

    projects page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+	selector: 'app-projects',
+	templateUrl: './projects.component.html',
+	styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	description: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis cupiditate tempore enim saepe ipsum velit id accusamus alias quaerat voluptatibus et temporibus ut suscipit, repellat similique facere nesciunt itaque eaque, exercitationem rerum distinctio. Accusamus numquam quas expedita at ea. Ipsum laborum sed, est veritatis aliquam distinctio iste ab libero officiis fugiat repudiandae, ducimus sunt at tempore ullam labore ipsam, harum neque repellendus architecto! Saepe, voluptatum a ratione excepturi ipsam officiis quasi, aliquid, dignissimos ab eveniet mollitia autem consectetur vitae aperiam id quos eius? Animi tenetur sed adipisci consectetur reiciendis enim, nam minus fugit doloremque eaque harum? Nostrum nam nisi natus cum veritatis excepturi! Natus, corrupti itaque? Fuga accusamus sint nam, aperiam nobis eos accusantium voluptatem quia recusandae in ipsam reiciendis, voluptates obcaecati ut. Earum praesentium neque dolorum porro dolore reiciendis repudiandae, maiores dignissimos quo quibusdam sunt debitis necessitatibus ex. Quia suscipit id, ab dolorum atque ratione distinctio consequatur totam debitis omnis maiores magnam reprehenderit animi, enim a autem nihil, quo dignissimos quas temporibus dolores libero? Dolore labore cupiditate sequi delectus velit quod consequuntur quae, tenetur ducimus mollitia perferendis deserunt, reiciendis excepturi! Eum officia, reiciendis architecto vitae libero ea nam fuga voluptate porro excepturi, minus, corporis fugiat. Fugit eum labore consequuntur.';
+
+	ngOnInit(): void {
+	}
 
 }
